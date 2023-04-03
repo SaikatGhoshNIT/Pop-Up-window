61b96223499adb7d7a8a9c44bbddc0e70987f117
Saikat Ghosh <saikatghosh5434@gmail.com>
2023-04-03 21:43:44 +0530

    feat: Basic structure and styling.

---
+++ script.js
@@ -0,0 +1,20 @@
+/*
+*TODO: 2.Select the elements with the following IDs
+*modal
+*open-modal-button
+*close-modal-button
+*BONUS: overlay
+*/
+
+const modal = document.getElementById("modal")
+const openModal = document.getElementById("open-modal-btn")
+const closeModal = document.getElementById("close-modal-btn")
+const overlay = document.getElementById("overlay")
+
+//TODO: 3. Create a click event Listener for the open-modal-btn that adds the class "open" to the overlay
+//BONUS: Also add the class "open" to the overlay
+
+//TODO: 4. Create a click event Listener for the close-modal-btn that removes the class "open" from the modal
+//BONUS: Also removes the class "open" from the overlay
+
+//BONUS: Add a click event listener to the overlay that removes the class "open" from the modal and  the overlay

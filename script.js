c1c5f81dcf327db69f16445a0a86311979711fe3
Saikat Ghosh <saikatghosh5434@gmail.com>
2023-04-04 20:23:07 +0530

    feat: on click of button pop-up should show, and on click pop-up should off

--- script.js
+++ script.js
@@ -11,10 +11,21 @@ const openModal = document.getElementById("open-modal-btn")
 const closeModal = document.getElementById("close-modal-btn")
 const overlay = document.getElementById("overlay")
 
-//TODO: 3. Create a click event Listener for the open-modal-btn that adds the class "open" to the overlay
+/*TODO: 3. Create a click event Listener for the open-modal-btn that adds the class "open" to the modal*/
+
 //BONUS: Also add the class "open" to the overlay
+openModal.addEventListener('click', ()=>{
+    modal.classList.add("open")
+    overlay.classList.add("open")
+    
+})
 
-//TODO: 4. Create a click event Listener for the close-modal-btn that removes the class "open" from the modal
+/*TODO: 4. Create a click event Listener for the close-modal-btn that removes the class "open" from the modal*/
+function close(){
+    modal.classList.remove("open")
+    overlay.classList.remove("open")
+}
 //BONUS: Also removes the class "open" from the overlay
-
+closeModal.addEventListener('click', close)
 //BONUS: Add a click event listener to the overlay that removes the class "open" from the modal and  the overlay
+overlay.addEventListener("click", close)
